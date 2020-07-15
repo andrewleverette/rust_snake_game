d0795a2d239b71a62d2638638560fc6aaac6ba89
andrewleverette <andrew.leverette@decisionsource.com>
2020-07-15 10:31:26 -0500

    Update draw methods

--- src/draw.rs
+++ src/draw.rs
@@ -1,7 +1,7 @@
 //! Contains definitions for drawing to the screen
 
-use piston_window::{rectangle, Context, G2d};
 use piston_window::types::Color;
+use piston_window::{rectangle, Context, G2d};
 
 const BLOCK_SIZE: f64 = 25.0;
 
@@ -13,5 +13,28 @@ pub fn to_coord(x: i32, y: i32) -> (f64, f64) {
 }
 
 pub fn draw_block(x: i32, y: i32, color: Color, ctx: &Context, g: &mut G2d) {
-    unimplemented!();
+    let (x, y) = to_coord(x, y);
+
+    rectangle(
+        color,
+        [x, y, BLOCK_SIZE, BLOCK_SIZE],
+        ctx.transform,
+        g,
+    );
+}
+
+pub fn draws_rectangle(x: i32, y: i32, width: i32, height: i32, color: Color, ctx: &Context, g: &mut G2d) {
+    let (x, y) = to_coord(x, y);
+
+    rectangle(
+        color,
+        [
+            x,
+            y,
+            BLOCK_SIZE * (width as f64),
+            BLOCK_SIZE * (height as f64),
+        ],
+        ctx.transform,
+        g
+    )
 }
